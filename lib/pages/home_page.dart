0871a836d3a9cffd7503f5a041a61efaa4b6c143
NUNNAVIJAYAKRISHNA <vijayakrishna.n213@gmail.com>
2025-08-21 22:08:15 +0530

    Tested Some UI changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-            'Home Page',
+            'Home',
             style: TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
